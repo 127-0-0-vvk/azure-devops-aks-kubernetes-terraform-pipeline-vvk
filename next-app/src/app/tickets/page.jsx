ceac7f1eb8e01da2db6918d00ef43a57d0674948
127-0-0vvk <vvkz.1818@gmail.com>
2023-09-11 16:09:31 -0400

    changed page

--- next-app/src/app/tickets/page.jsx
+++ next-app/src/app/tickets/page.jsx
@@ -5,7 +5,7 @@ export default function Tickets() {
     <main>
         <h2>Tickets</h2>
         <h3>View Tickets</h3>
-        <p>Change is not here</p>
+        <p>Change is here</p>
     </main>
   )
 }
